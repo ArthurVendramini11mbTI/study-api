37535f3ca3bbad1f9514172fe4f10497db027fb7
ArthurVendra <vendraminiarthur2008@gmail.com>
2026-09-05 18:54:36 -0300

    Definido o schema das Goals e ajustes no schema do User

--- src/schemas/userSchemas.ts
+++ src/schemas/userSchemas.ts
@@ -1,7 +1,44 @@
 import * as z from 'zod'
 
-export const User = z.object({
-    email: z.string().regex(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?)*$/),
-    password: z.string(),
-    name: z.string()
+const Time = z.object({
+    hours: z.number(),
+    minutes: z.number()
 })
+
+export const Goal = z.object({
+  name: z.string(),
+  description: z.string(),
+  time: z.object({
+    totalTime: Time,
+    timeSpent: Time,
+  }),
+  icon: z.string(),
+  color: z.string()
+}).transform((data) => {
+  const totalMinutes = data.time.totalTime.hours * 60 + data.time.totalTime.minutes
+
+  const spentMinutes = data.time.timeSpent.hours * 60 + data.time.timeSpent.minutes
+
+  const timeLeft = Math.max(totalMinutes - spentMinutes, 0)
+
+  const progress = totalMinutes === 0 ? 0 : Number(Math.min((spentMinutes / totalMinutes) * 100, 100)).toFixed(2)
+
+  return {
+    ...data,
+
+    time: {
+      ...data.time,
+      timeLeft,
+    },
+
+    progress,
+  }
+})
+
+export const User = z.object({
+    email: z.email(),
+    password: z.string(),
+    name: z.string().optional(),
+    goals: z.array(Goal).optional()
+})
+
--- src/types/userTypes.ts
+++ src/types/userTypes.ts
@@ -2,3 +2,4 @@ import type { User } from '../schemas/userSchemas'
 import * as z from 'zod'
 
 type User = z.infer<typeof User>
+
